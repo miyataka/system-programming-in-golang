fc65500d68878218681854355da11cde2f69401c
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-07-03 14:16:07 +0900

    2.4.5.c: network io

--- main.go
+++ main.go
@@ -2,18 +2,14 @@ package main
 
 import (
 	"io"
-	"net"
 	"net/http"
-	"os"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "http.ResponseWriter sample")
+}
+
 func main() {
-	conn, err := net.Dial("tcp", "ascii.jp:80")
-	if err != nil {
-		panic(err)
-	}
-	io.WriteString(conn, "GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n")
-	io.Copy(os.Stdout, conn)
-	req, err := http.NewRequest("GET", "http://ascii.jp", nil)
-	req.Write(conn)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
