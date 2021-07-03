552decb77f76c2c80904c1af569d844d1736f961
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-07-03 14:25:26 +0900

    2.4.6.b: gzip.Writer

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"compress/gzip"
 	"io"
 	"os"
 )
 
 func main() {
-	file, err := os.Create("multiwriter.txt")
+	file, err := os.Create("test.txt.gz")
 	if err != nil {
 		panic(err)
 	}
-	writer := io.MultiWriter(file)
-	io.WriteString(writer, "io.MultiWriter example\n")
+	writer := gzip.NewWriter(file)
+	writer.Header.Name = "test.txt"
+	io.WriteString(writer, "gzip.Writer example\n")
+	writer.Close()
 }
