00e14ce1a8fc7f4f385b804dbb0350c17488e6d2
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-07-10 23:06:12 +0900

    3.7.b io.TeeReader

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"io"
-	"os"
+	"io/ioutil"
 )
 
 func main() {
-	header := bytes.NewBufferString("---- HEADER ----\n")
-	content := bytes.NewBufferString("Example of io.MultiReader\n")
-	footer := bytes.NewBufferString("---- FOOTER ----\n")
-
-	reader := io.MultiReader(header, content, footer)
-	io.Copy(os.Stdout, reader)
+	var buffer bytes.Buffer
+	reader := bytes.NewBufferString("Example of io.TeeReader\n")
+	teeReader := io.TeeReader(reader, &buffer)
+	_, _ = ioutil.ReadAll(teeReader)
+	fmt.Println(buffer.String())
 }
