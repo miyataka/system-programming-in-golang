8093754ff6c5f4cfde4dd564be1f089d3c1df56d
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-07-11 16:08:52 +0900

    4.1.b goroutine with anonymous function

--- main.go
+++ main.go
@@ -5,14 +5,12 @@ import (
 	"time"
 )
 
-func sub() {
-	fmt.Println("sub() is running")
-	time.Sleep(time.Second)
-	fmt.Println("sub() is finished")
-}
-
 func main() {
 	fmt.Println("start sub()")
-	go sub()
+	go func() {
+		fmt.Println("sub() is running")
+		time.Sleep(time.Second)
+		fmt.Println("sub() is finished")
+	}()
 	time.Sleep(2 * time.Second)
 }
