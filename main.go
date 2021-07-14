0de676e607605b237c9142fd1b47d2a684615cf9
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-07-15 00:41:30 +0900

    6.7.2: gzip compressing support http server

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"time"
+)
+
+// if client accept gzipped response?
+func isGZipAcceptable(request *http.Request) bool {
+	return strings.Index(strings.Join(request.Header["Accept-Encoding"], ","), "gzip") != -1
+}
+
+// 1 session procedure
+func processSession(conn net.Conn) {
+	fmt.Printf("Accept %v\n", conn.RemoteAddr())
+	defer conn.Close()
+	for {
+		// set timeout
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+
+		// read request
+		request, err := http.ReadRequest(bufio.NewReader(conn))
+		if err != nil {
+			// timeout or socket close
+			neterr, ok := err.(net.Error) // down cast
+			if ok && neterr.Timeout() {
+				fmt.Println("Timeout")
+				break
+			} else if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		// display request
+		dump, err := httputil.DumpRequest(request, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(dump))
+
+		// write response
+		// HTTP/1.1 and ContentLength
+		response := http.Response{
+			StatusCode: 200,
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+			Header:     make(http.Header),
+		}
+		if isGZipAcceptable(request) {
+			content := "Hello World (gzipped)\n"
+			var buffer bytes.Buffer
+			writer := gzip.NewWriter(&buffer)
+			io.WriteString(writer, content)
+			writer.Close()
+			response.Body = ioutil.NopCloser(&buffer)
+			response.ContentLength = int64(buffer.Len())
+			response.Header.Set("Content-Encoding", "gzip")
+		} else {
+			content := "Hello World\n"
+			response.Body = ioutil.NopCloser(strings.NewReader(content))
+			response.ContentLength = int64(len(content))
+		}
+		response.Write(conn)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Server is running at localhost:8888")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go processSession(conn)
+	}
+}
