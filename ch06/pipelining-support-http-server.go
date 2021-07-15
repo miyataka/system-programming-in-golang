ab99e85874a9b34f825f9b99b6d033bb8871d8ff
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-07-15 22:24:53 +0900

    fix: 6.9.1 pipelining server

--- ch06/pipelining-support-http-server.go
+++ ch06/pipelining-support-http-server.go
@@ -80,48 +80,8 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
+		defer conn.Close()
 
-		go func() {
-			defer conn.Close()
-			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-			for {
-				// set timeout
-				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-
-				// read request
-				request, err := http.ReadRequest(bufio.NewReader(conn))
-				if err != nil {
-					// timeout or socket close
-					neterr, ok := err.(net.Error) // down cast
-					if ok && neterr.Timeout() {
-						fmt.Println("Timeout")
-						break
-					} else if err == io.EOF {
-						break
-					}
-					panic(err)
-				}
-
-				// display request
-				dump, err := httputil.DumpRequest(request, true)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(string(dump))
-				content := "Hello World\n"
-
-				// write response
-				// HTTP/1.1 and ContentLength
-				response := http.Response{
-					StatusCode:    200,
-					ProtoMajor:    1,
-					ProtoMinor:    1,
-					ContentLength: int64(len(content)),
-					Body:          ioutil.NopCloser(strings.NewReader("Hello World\n")),
-				}
-
-				response.Write(conn)
-			}
-		}()
+		go processSession(conn)
 	}
 }
