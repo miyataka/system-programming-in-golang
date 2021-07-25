8b6a9be772613f5550752d540d73998e841a7e42
Takayuki Miyahara <voyager.3taka28@gmail.com>
2021-07-25 18:30:50 +0900

    13.7.2 sync-waitgroup

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go func() {
+		fmt.Println("work 1")
+		wg.Done()
+	}()
+
+	go func() {
+		fmt.Println("work 2")
+		wg.Done()
+	}()
+
+	wg.Wait()
+	fmt.Println("DONE")
+}
